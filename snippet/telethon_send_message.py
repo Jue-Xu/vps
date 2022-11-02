8b04fa0201a039f2070d1ce653b4c657ea8379c6
Jue Xu <xujuexujue@hotmail.com>
2022-11-02 20:24:58 +0800

    update yml, rec, crontab; telethon

---
+++ snippet/telethon_send_message.py
@@ -0,0 +1,20 @@
+from telethon import TelegramClient, events, sync
+import time
+
+# These example values won't work. You must get your own api_id and
+# api_hash from https://my.telegram.org, under API Development.
+api_id = 
+api_hash = '9841a9fe685d6f1463e0d85ad88e42f3'
+
+client = TelegramClient('session_name', api_id, api_hash)
+client.start()
+
+print(client.get_me().stringify())
+
+group_links = ['https://t.me/NakoNetwork','https://t.me/Singularity_Emby_Group']
+
+for link in group_links:
+    entity=client.get_entity(link)
+    client.send_message(entity=entity, message='签到')
+    time.sleep(10)
+    #client.send_message('arthurmerlin', '签到')
